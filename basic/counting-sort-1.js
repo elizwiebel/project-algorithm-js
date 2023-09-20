12f0d6599251db6df0420fe6984c13e2c9a7cffc
Eli Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-09-20 18:30:16 +0200

    Add basic counting sort 1 problem solution

---
+++ basic/counting-sort-1.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/*
+INSTRUCTIONS
+
+    Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+    Complete the 'countingSort' function below.
+
+NOTES
+
+    INPUT DATA
+
+        Input is an array of integers
+
+        EXAMPLE INPUT DATA
+            [63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33]
+
+    OUTPUT DATA
+
+        Output is a count of each integer starting at 0 to the max value
+
+        EXAMPLE OUTPUT DATA
+            [ 0, 2, 0, 2, 0, 0, 1, 0, 1, 2, 1, 0, 1, 1, 0, 0, 2, 0, 1, 0, 1, 2, 1, 1, 1, 3, 0, 2, 0, 0, 2, 0, 3, 3, 1, 0, 0, 0, 0, 2, 2, 1, 1, 1, 2, 0, 2, 0, 1, 0, 1, 0, 0, 1, 0, 0, 2, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 2, 1, 3, 2, 0, 0, 2, 1, 2, 1, 0, 2, 2, 1, 2, 1, 2, 1, 1, 2, 2, 0, 3, 2, 1, 1, 0, 1, 1, 1, 0, 2, 2]
+*/
+
+const countingSort = (inputArray) => {
+    console.log("inputArray", inputArray)
+
+    const maxValue = Math.max(...inputArray)
+    console.log("maxValue", maxValue)
+
+    const countArray = new Array(maxValue + 1).fill(0)
+    console.log("initial countArray", countArray)
+
+    for (const element of inputArray) {
+        // Increment current element count in count array
+        countArray[element]++
+    }
+
+    console.log("completed countArray", countArray)
+
+    return countArray
+}
+
+const inputArray = [63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33]
+
+countingSort(inputArray)
