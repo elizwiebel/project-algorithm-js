6291b9d8bb46e5369007e0cbd104770f91d96bab
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-11-19 17:15:38 -0700

    Add basic fizz buzz solution

---
+++ basic/fizz-buzz.js
@@ -0,0 +1,52 @@
+'use strict'
+
+/*
+INSTRUCTIONS
+
+    Print out the numbers from 1 - 20.
+
+    For numbers divisible by 3, print out “Fizz”.
+    For numbers divisible by 5, print out “Buzz”.
+    For numbers divisible by both 3 and 5, print out “FizzBuzz” in the console.
+    Otherwise, just print out the number.
+
+NOTES
+
+    INPUT
+        const input = 100
+
+    OUTPUT
+        1
+        2
+        Fizz
+        4
+        Buzz
+        Fizz
+        7
+        8
+        Fizz
+        Buzz
+        11
+        Fizz
+        13
+        14
+        FizzBuzz
+*/
+
+const fizzBuzz = (input) => {
+    for (let i = 1; i < input + 1; i++) {
+        if (i % 15 === 0) {
+            console.log('FizzBuzz')
+        } else if (i % 3 === 0) {
+            console.log('Fizz')
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+const input = 20
+
+console.log(fizzBuzz(input))
