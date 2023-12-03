3f324d7605dd969b2ba7c48322564928e0ebb00b
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-12-02 20:23:10 -0700

    Add highest average score solution

--- misc/highest-average-score.js
+++ misc/highest-average-score.js
@@ -12,7 +12,46 @@ OUTPUT
     99
 */
 
-function highestAverage(arr) {}
+function highestAverage(arr) {
+    const studentHashTable = {};
+    let highestAverage = 0;
+    let bestStudent = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        const [name, score] = Object.entries(arr[i])[0];
+        const scoreNum = parseInt(score);
+        console.log("i:", i);
+        console.log("   name:", name);
+        console.log("   score:", scoreNum);
+
+        if (studentHashTable[name]) {
+            studentHashTable[name].scores.push(scoreNum);
+        } else {
+            studentHashTable[name] = { scores: [scoreNum] };
+        }
+
+        const sum = studentHashTable[name].scores.reduce((currentSum, currentScore) => {
+            return currentSum + currentScore;
+        }, 0);
+
+        const average = sum / studentHashTable[name].scores.length;
+
+        studentHashTable[name].average = average;
+
+        if (average > highestAverage) {
+            highestAverage = average;
+            bestStudent = name;
+        }
+
+        console.log("   average:", average);
+    }
+
+    console.log("studentHashTable:", studentHashTable);
+    console.log("highestAverage:", highestAverage);
+    console.log("bestStudent:", bestStudent);
+
+    return { [bestStudent]: highestAverage };
+}
 
 const input = [{ Bob: "87" }, { Mike: "35" }, { Bob: "52" }, { Jason: "35" }, { Mike: "55" }, { Jessica: "99" }];
 
