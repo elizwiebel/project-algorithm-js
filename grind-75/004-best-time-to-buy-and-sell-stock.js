1b15d3f13ec6a6a9c43fc51b5bcd25a8a467ccf2
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-12-05 12:08:12 -0700

    Add best time to sell stock solution

--- grind-75/004-best-time-to-buy-and-sell-stock.js
+++ grind-75/004-best-time-to-buy-and-sell-stock.js
@@ -10,4 +10,25 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {};
+var maxProfit = function (prices) {
+    let maxProfit = 0;
+    let minBuyPrice = prices[0];
+
+    for (let i = 0; i < prices.length; i++) {
+        console.log("i:", i);
+        const currentPrice = prices[i];
+        const currentProfit = currentPrice - minBuyPrice;
+
+        console.log("   currentPrice:", currentPrice);
+        console.log("   minBuyPrice:", minBuyPrice);
+        console.log("   currentProfit:", currentProfit);
+        console.log("   maxProfit:", maxProfit);
+
+        if (currentProfit > maxProfit) maxProfit = currentProfit;
+        if (currentPrice < minBuyPrice) minBuyPrice = currentPrice;
+    }
+
+    return maxProfit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
