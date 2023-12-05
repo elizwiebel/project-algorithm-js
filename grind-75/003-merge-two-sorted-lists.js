277eea8385f0c6add435e1d76b9fcc5873e296d9
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-12-05 11:44:58 -0700

    Add merge two sorted lists solution

--- grind-75/003-merge-two-sorted-lists.js
+++ grind-75/003-merge-two-sorted-lists.js
@@ -18,4 +18,26 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {};
+var mergeTwoLists = function (list1, list2) {
+    let dummy = new ListNode(0);
+    let head = dummy;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            dummy.next = list1;
+            list1 = list1.next;
+        } else {
+            dummy.next = list2;
+            list2 = list2.next;
+        }
+        dummy = dummy.next;
+    }
+
+    if (list1 !== null) {
+        dummy.next = list1;
+    } else {
+        dummy.next = list2;
+    }
+
+    return head.next;
+};
