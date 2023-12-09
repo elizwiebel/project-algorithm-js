e773c572f735e18fb9c6c6eb53bc9c58d1e348bd
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-12-09 11:11:15 -0700

    Add valid anagram solution

--- grind-75/007-valid-anagram.js
+++ grind-75/007-valid-anagram.js
@@ -11,4 +11,13 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function (s, t) {};
+var isAnagram = function (s, t) {
+    s = s.split("").sort().join("");
+    t = t.split("").sort().join("");
+
+    if (s === t) return true;
+
+    return false;
+};
+
+console.log(isAnagram("anagram", "nagaram"));
